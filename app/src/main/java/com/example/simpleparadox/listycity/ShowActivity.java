27e49e81bceb9725dfcc926dc885b0be22926a5a
Samir <samirsaif07@gmail.com>
2022-01-25 23:52:55 +0600

    assignment

---
+++ app/src/main/java/com/example/simpleparadox/listycity/ShowActivity.java
@@ -0,0 +1,14 @@
+package com.example.simpleparadox.listycity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+    }
+}
